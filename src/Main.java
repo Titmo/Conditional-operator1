f620e1a6867fa43ce91a6e43b64e3bd9cfee1a7b
Danila <03denchik03@gmail.com>
2023-03-13 19:08:21 +0300

    Сделанные первые 3 задания!

---
+++ src/Main.java
@@ -0,0 +1,58 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        byte age = 19;
+        if (age>=18){
+            System.out.println("Если возраст человека равен " +age+ ", то он совершеннолетний");
+        }
+        else {
+            System.out.println("Если возраст человека равен " +age+ ", он не достиг совершеннолетия, нужно немного подождать");
+        }
+    }
+    public static void task2() {
+        System.out.println("Задача 2");
+        byte temperature = -23;
+        if (temperature<=5){
+            System.out.println("На улице холодно, нужно надеть шапку");
+        }
+        else {
+            System.out.println("Сегодня тепло, можно идти без шапки");
+        }
+    }
+    public static void task3() {
+        System.out.println("Задача 3");
+        byte seed = 60;
+        if (seed<=60){
+            System.out.println("Если скорость "+seed+" , то можно ездить спокойно");
+        }
+        else {
+            System.out.println("Если скорость "+seed+" , то придется заплатить штраф");
+        }
+    }
+    public static void task4() {
+        System.out.println("Задача 4");
+
+    }
+    public static void task5() {
+        System.out.println("Задача 5");
+
+    }
+    public static void task6() {
+        System.out.println("Задача 6");
+
+    }
+    public static void task7() {
+        System.out.println("Задача 7");
+
+    }
+}
